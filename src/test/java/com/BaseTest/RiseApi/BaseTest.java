03341d6604e9dafb9df1eecec98c60af94dec1f7
Shreyas <Shreyas@ShreyasS-W10>
2022-04-04 16:43:17 +0530

    added login api for resident and also shifted all variables in BaseTest class

--- src/test/java/com/BaseTest/RiseApi/BaseTest.java
+++ src/test/java/com/BaseTest/RiseApi/BaseTest.java
@@ -7,10 +7,34 @@ import java.util.Properties;
 
 import org.testng.annotations.BeforeSuite;
 
+import com.google.gson.JsonObject;
+
 import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
 
 public class BaseTest {
 
+	public static String ConvertedResponse = null;
+	public static String pm_token = null;
+	public static String webstaff_staff_token = null;
+	public static String webstaff_staff_id = null;
+	public static String webstaff_staff_RoleId = null;
+	public static String AuthToken = null;
+	public static String user_id = null;
+	public static JsonObject jObject = null;
+	public static JsonObject childObject = null;
+	public static RequestSpecification httpRequest = null;
+	public static Response response = null;
+	public static String responseMessage = null;
+	public static String resident_token = null;
+	public static String resident_id = null;
+	public static String unit_id = null;
+	public static String f_name = null;
+	public static String l_name = null;
+	public static String unit_no = null;
+	public static String resident_roleId = null;
+	public static int statusCode;
 	public static Properties prop;
 
 	@BeforeSuite
--- src/test/java/com/Login/RiseApi/LoginApisTest.java
+++ src/test/java/com/Login/RiseApi/LoginApisTest.java
@@ -14,20 +14,6 @@ import io.restassured.specification.RequestSpecification;
 
 public class LoginApisTest extends BaseTest {
 
-	String ConvertedResponse = null;
-	String pm_token = null;
-	String webstaff_staff_token = null;
-	String webstaff_staff_id = null;
-	String webstaff_staff_RoleId = null;
-	String AuthToken = null;
-	String user_id = null;
-	JsonObject jObject = null;
-	JsonObject childObject = null;
-	RequestSpecification httpRequest = null;
-	Response response = null;
-	String responseMessage = null;
-	int statusCode;
-
 	@Test(priority = 1, enabled = true)
 	public void Login_WEBPM() {
 
@@ -54,6 +40,38 @@ public class LoginApisTest extends BaseTest {
 		response = httpRequest.given().contentType("application/json").when().post("/api/login");
 		ConvertedResponse = response.getBody().asString();
 		jObject = new Gson().fromJson(ConvertedResponse, JsonObject.class);
+		childObject = jObject.getAsJsonObject("data");
+
+		webstaff_staff_token = childObject.get("token").getAsString(); // get token
+		webstaff_staff_id = childObject.get("user_id").getAsString();
+		webstaff_staff_RoleId = childObject.get("StaffRoles").getAsString();
+
+		statusCode = response.getStatusCode();
+		responseMessage = jObject.get("msg").getAsString();
+		Assert.assertEquals(statusCode /* actual value */, 200/* expected value */, "Correct status code returned");
+		Assert.assertEquals(responseMessage, "Login Successfully.", "Correct response message returned");
+
+	}
+
+	@Test(priority = 3, enabled = true)
+	public void Login_WEBResident() {
+
+		httpRequest = RestAssured.given();
+		httpRequest.queryParam("email", prop.getProperty("resident_username"));
+		httpRequest.queryParam("password", prop.getProperty("resident_password"));
+		response = httpRequest.given().contentType("application/json").when().post("/api/login");
+		ConvertedResponse = response.getBody().asString();
+		jObject = new Gson().fromJson(ConvertedResponse, JsonObject.class);
+		childObject = jObject.getAsJsonObject("data");
+
+		resident_token = childObject.get("token").getAsString(); // get token
+		resident_id = childObject.get("user_id").getAsString();
+		unit_id = childObject.get("units_id").getAsString();
+		f_name = childObject.get("firstname").getAsString();
+		l_name = childObject.get("lastname").getAsString();
+		unit_no = childObject.get("unit_number").getAsString();
+		resident_roleId = childObject.get("ResidentRoles").getAsString();
+
 		statusCode = response.getStatusCode();
 		responseMessage = jObject.get("msg").getAsString();
 		Assert.assertEquals(statusCode /* actual value */, 200/* expected value */, "Correct status code returned");
